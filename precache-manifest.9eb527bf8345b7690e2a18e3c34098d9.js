8855d85dedbc301140875af04ee998805cecfff0
Joel Detweiler <joel.detweiler@gmail.com>
2019-03-26 15:17:10 -0400

    Updates

---
+++ precache-manifest.9eb527bf8345b7690e2a18e3c34098d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5afebdff067e11519623",
+    "url": "/static/js/main.e71abb07.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5afebdff067e11519623",
+    "url": "/static/css/main.36ffd87b.chunk.css"
+  },
+  {
+    "revision": "a9bc5a3aa78559b01feb219c376b4d3b",
+    "url": "/index.html"
+  }
+];
